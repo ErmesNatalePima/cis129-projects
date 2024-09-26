125e6b779bc10108f92073a318bdc4519825ac24
ErmesNatalePima <enatale@mail.pima.edu>
2024-09-25 20:46:48 -0700

    Update cis129_lab02_shortStory.py
    added Ronnie Coleman story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Author: Ermes Natale
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+# deleted previous entry
+print('LIGHT WEIGHT!') # starting with inspiration quotes from Ronnie
+print('Yeah buddy, lift heavy or go home.')
+print("Yeah buddy, I don't just lift light weight, I lift heavy and I lift often.")
+print("Ronald Dean Coleman (born May 13, 1964) is an American retired professional bodybuilder. Known as 'The King', Coleman shares the all-time record for most Mr. Olympia titles at eight with Lee Haney. The winner of the Mr. Olympia title for eight consecutive years, he is generally regarded as the greatest professional bodybuilder of all time.") # pulled from Wiki page
